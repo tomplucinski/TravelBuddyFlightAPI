e518d6c050a6d384dcc69554534be8a68f4d49fd
Tom Plucinski <tapluci1@gmail.com>
2020-08-22 14:39:48 -0500

    added getquotes endpoint

--- src/main/kotlin/com/travelbuddy/api/FlightDataController.kt
+++ src/main/kotlin/com/travelbuddy/api/FlightDataController.kt
@@ -10,6 +10,7 @@ import java.net.http.HttpRequest
 import java.net.http.HttpResponse
 import com.travelbuddy.utils.Constants
 import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.web.bind.annotation.RequestParam
 
 @RestController
 class FlightDataController {
@@ -20,14 +21,31 @@ class FlightDataController {
     @GetMapping("/getairports")
     fun getAirportData(): ResponseEntity<String>? {
 
-        val client = HttpClient.newBuilder().build();
+        val client = HttpClient.newBuilder().build()
         val request = HttpRequest.newBuilder()
                 .uri(URI.create(String.format("%s%s", Constants.AIRPORT_DATA, "USA/USD/en-US/?query=chicago")))
                 .setHeader("x-rapidapi-host", "skyscanner-skyscanner-flight-search-v1.p.rapidapi.com")
                 .setHeader("x-rapidapi-key", appProperties.rapidApiKey)
                 .setHeader("Content-Type", "application/json")
-                .build();
-        val response = client.send(request, HttpResponse.BodyHandlers.ofString());
+                .build()
+
+        val response = client.send(request, HttpResponse.BodyHandlers.ofString())
+
+        return ResponseEntity.ok(response.body().toString())
+    }
+
+    @GetMapping("/getquotes")
+    fun getQuotes(@RequestParam origin: String, destination: String, date: String): ResponseEntity<String>? {
+
+        val client = HttpClient.newBuilder().build()
+        val request = HttpRequest.newBuilder()
+                .uri(URI.create(String.format("%s%s%s/%s/%s", Constants.QUOTES, "US/USD/en-US/", origin, destination, date)))
+                .setHeader("x-rapidapi-host", "skyscanner-skyscanner-flight-search-v1.p.rapidapi.com")
+                .setHeader("x-rapidapi-key", appProperties.rapidApiKey)
+                .setHeader("Content-Type", "application/json")
+                .build()
+
+        val response = client.send(request, HttpResponse.BodyHandlers.ofString())
 
         return ResponseEntity.ok(response.body().toString())
     }
--- src/main/kotlin/com/travelbuddy/utils/Constants.kt
+++ src/main/kotlin/com/travelbuddy/utils/Constants.kt
@@ -2,4 +2,5 @@ package com.travelbuddy.utils
 
 object Constants {
     const val AIRPORT_DATA = "https://skyscanner-skyscanner-flight-search-v1.p.rapidapi.com/apiservices/autosuggest/v1.0/"
+    const val QUOTES = "https://skyscanner-skyscanner-flight-search-v1.p.rapidapi.com/apiservices/browsequotes/v1.0/"
 }
